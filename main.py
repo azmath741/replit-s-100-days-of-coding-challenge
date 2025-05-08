8f47cbb5fbc53564b44203b15a922283ed98d27d
azmath741 <azmathali76636@gmail.com>
2025-05-08 11:27:09 +0530

    100 days of coding challenge (day-1)

---
+++ main.py
@@ -0,0 +1,10 @@
+print("""
+Azmath
+may 8 2025
+I a signing up for replit's 100 days of  python challenge!
+I will make sure to spend some time every day coding along, for a minimum of 10 minutes a day.
+I'll be using Replit, an amazing online IDE so I can do this from my phone wherever I happen to be. No excuses for not coding from the middle of a field!
+I am feeling ðŸ¤©
+You can follow my progress at replit.com/@Azmath  
+
+""")

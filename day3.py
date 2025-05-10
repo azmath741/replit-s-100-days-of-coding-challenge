01189bfff5e5b2af278d0527c91cb246aab11802
azmath741 <azmathali76636@gmail.com>
2025-05-10 19:29:33 +0530

    100 days of coding challenge (day3)

--- day3.py
+++ day3.py
@@ -1,7 +1,17 @@
+# let's come up with a recipe generator to build us an amazing dish for today's evening meal!
+# You will need to:
+# 1.Create these as a variable:
+#   A type of food
+#   A type of plant
+#   A method of cooking
+#   A word to describe burned food
+#   A household item
+# 2. Output a nice looking Recipe page that *concatenates* a dish in this format:
+# cooking food with burned plant on a bed of item
 food_type=input("Enter food type: ")
 plant_name=input("type of plant:")
 method=input("A method of cooking:")
 describe=input("describe the food:")
 item=input("item:")
-print(f"I love {food_type}! It is a {describe} dish that contains {item}.")
-print(f"The key to a perfect {food_type} is {method}")
+print("I love! ",food_type," It is a ",describe," dish that contains ","item.")
+print("The key to a perfect", food_type," is", method)

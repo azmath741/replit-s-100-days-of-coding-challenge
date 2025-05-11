4e963ba1cd038b16d52c694b32c3d92e63613494
azmath741 <azmathali76636@gmail.com>
2025-05-11 23:23:28 +0530

    100 days of code challenge(day4)

---
+++ day4.py
@@ -0,0 +1,9 @@
+print("""Welcome to your adventure simulator. I am going to ask you a bunch of questions and then create an epic story with you as the star!""")
+name=input("What is your name?")
+enemy=input("What is your worst enemy's name?")
+superpower=input("What is your superpower?")
+location=input("Where do you live?")
+game=input("What is your favorite game to play?")
+food=input("What is your favorite food?")
+
+print(f"Hello {name}! Your ability to {superpower} will make sure you never have to look at {enemy} again. Go eat {food} as you walk down the streets of {location} and use {superpower} for good and not evil!and think it as a {game} game in real life")

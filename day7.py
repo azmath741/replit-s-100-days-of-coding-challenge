c841e1d0531fe18bcd0bd5e85da8053660bb11a3
azmath741 <azmathali76636@gmail.com>
2025-05-15 00:08:01 +0530

    100 days of coding challenge(day7)

--- day7.py
+++ day7.py
@@ -1,16 +1,19 @@
-genre=input("which genre do you like most? action or comedy :").lower()
+genre=input("which genre do you like most? action or horror/comedy :").lower()
 print()
 if genre == "action":
-    print("you should watch the movie 'Die Hard'")   
-    if genre != "action":
-        print("you are not  a super fan of action    movies")
+    print("you should watch the movie'Bahubali'") 
+    hero= input("who is a main role in the bahubali movie  ? prabhas or ranveer singh :").lower()
+    if  hero == "prabhas":
+      print("you are  a super fan of action movies")
+
     else:
-      print("you are a super fan of action movies")
-elif genre == "comedy":
-    print("you should watch the movie 'Free Guy'")
-    if genre != "comedy":
-        print("you are not  a super fan of comedy movies")
+      print("you are not a super fan of action movies")
+elif genre == "horror/comedy":
+    print("you should watch the movie 'stree 2'")
+    hero= input("who is the man that shown in the climex of the movie  ? varun dhawan or ram charan :").lower()
+    if hero == "varun dhawan":
+        print("you are  a super fan of horror/comedy movies")
     else:
-      print("you are a super fan of comedy movies")
+      print("you are not a super fan of horror/comedy movies")
 else:
   print("invalid genre")
